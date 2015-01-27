84f408fbfec1e7ac53c1f5fa7202d4d21e19b244
Luis Felipe <lfna23@gmail.com>
2015-01-27 17:44:21 -0500

    feat(Nombre): Paso de parametros entre activity

--- src/com/example/myapp/MainActivity.java
+++ src/com/example/myapp/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.myapp;
 
+
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity implements OnClickListener {
+	private Button btn_enviar;
+	private EditText txt_name;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btn_enviar = (Button) findViewById(R.id.btn_enviar);
+		btn_enviar.setOnClickListener(this);
 	}
 
 	@Override
@@ -30,5 +39,15 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		//startActivity(new Intent(MainActivity.this, HomeActivity.class));
+		txt_name = (EditText) findViewById(R.id.txt_name);
+		Intent open_home = new Intent(this, HomeActivity.class);
+		open_home.putExtra("txt_name", txt_name.getText().toString());
+		startActivity(open_home);
 	}
 }
